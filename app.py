d0ffec0314505091b4079c74c53c019f0869f238
simutek <simutek@Xavier-Mac-3.local>
2024-01-30 16:14:01 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    #lauch browser
+    browser = playwright.chromium.launch(headless=False, slow_mo= 500) #to see the interface
+    #create a new page
+    page = browser.new_page()
+    #visit playwright website
+    page.goto("https://playwright.dev/python")
+    #locate a link element with "Docs" text
+    docs_button = page.get_by_role('link', name="Docs")
+    docs_button.click()
+    #get url
+
+
+    browser.close()
