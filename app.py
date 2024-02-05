bb4eb6c07a7d4608df67603f564f1a5eadad5d83
simutek <simutek@Xavier-Mac-3.local>
2024-02-05 12:49:23 -0800

    feat:auto waiting

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ with sync_playwright() as playwright:
     #create a new page
     page = browser.new_page()
     #visit playwright website
-    page.goto("https://playwright.dev/python")
-    #locate a link element with "Docs" text
-    docs_button = page.get_by_role('link', name="Docs")
-    docs_button.click()
-    #get url and print it
-    print("Docs:", page.url)
+    page.goto("https://bootswatch.com/default")
 
-    #locator
-    docs_button= page.get_by_role('button', name= "Sign Up")
-    docs_button.click()
+    #locate a by role
+    button = page.get_by_role("button", name="Primary").firstbutton.click()
+    button.click()
+    #get url 
+    #print("Docs:", page.url)
 
     browser.close()
