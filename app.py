ab5ff6329a41385a4b24647579648d7caa09cdc3
simutek <simutek@Xavier-Mac-3.local>
2024-02-05 19:38:13 -0800

    feat: add page load event handling with Playwright
    Add a callback function  to handle the load event on a Playwright page. The callback function prints a message when the page is loaded

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from playwright.sync_api import sync_playwright
-from time import perf_counter #gives us time stamps 
+
+
+def on_load(page):
+    print("Page loaded", page)
 
 with sync_playwright() as playwright:
     #lauch browser
     browser = playwright.chromium.launch(headless=False, slow_mo= 500) #to see the interface
     #create a new page
     page = browser.new_page()
-    print("Page loading...")
-    start = perf_counter()
 
-    page.goto(
-        "https://bootswatch.com/default",
-        wait_until='networkidle', #when you want to wait for the page to be relatively stable, and most network requests (like fetching additional resources) have settled down
-        )
 
-    time_taken = perf_counter() - start
-    print(f"...Page loaded in {round(time_taken, 2)}s")
-    # 2 decimal places
+    page.on("load", on_load)
+    page.goto("https://bootswatch.com/default")
+
     browser.close()
