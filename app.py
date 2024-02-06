18463673eb9ce263119a607290368618fff07684
simutek <simutek@Xavier-Mac-3.local>
2024-02-06 10:54:29 -0800

    feat:async playwright

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 
 
-def on_request(request):
-    print("Page loaded", page)
+async def main():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
+        page = await browser.new_page()
 
-with sync_playwright() as playwright:
-    #lauch browser
-    browser = playwright.chromium.launch(headless=False, slow_mo= 500) #to see the interface
-    #create a new page
-    page = browser.new_page()
+        await page.goto("https://playwright.dev")
+
+        print(await page.title())
+
+        await browser.close()
 
 
-    page.on("request", on_request)
-    page.goto("https://bootswatch.com/default")
-
-    browser.close()
+asyncio.run(main())
