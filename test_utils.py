af4d5b3f560c99f0cf634976c0113d1e60775633
simutek <simutek@Xavier-Mac-3.local>
2024-02-07 06:13:56 -0800

    test: root for pytest

--- test_utils.py
+++ test_utils.py
@@ -2,7 +2,9 @@ import utils
 
 
 def test_root():
+    utils.root_25
     print("Testing for 25...")
     root_25 = utils.root(25)
     print("...got ", root_25)
     assert root_25 == 5
+
--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+#by default python has a dynamic typing nature, at any time ypu can change the value of the number to a different type like a string, etc.
+num: int = 100
+lst: list[int] = [1, 2, 3, 4]
+lst.append("str")
+dt: dict[str, int] = {"key": 0 }
+
 
 def root(num):
     return pow(num, .5)
