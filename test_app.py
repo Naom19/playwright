f3d1ebc4d54d25b90c48b5d4eaa857c7b9259af5
simutek <simutek@Xavier-Mac-3.local>
2024-02-07 12:59:46 -0800

    test: records video

--- test_app.py
+++ test_app.py
@@ -1,15 +1,25 @@
 #test_app.py
 
 import pytest
-from playwright.sync_api import Browser, Page
+from playwright.sync_api import BrowserContext, Page
 
 DOCS_URL = "https://playwright.dev/python/docs/intro"
 
-
-def test_page_has_get_started_link(browser: Browser):
-    context = browser.new_context(
-        record_video_dir="video/"
+@pytest.fixture(autouse=True)
+def trace_test(context: BrowserContext):
+    context.tracing.start(
+        name="playwright",
+        screenshots=True,
+        snapshots=True,
+        sources=True,
     )
+    yield
+    context.tracing.stop(path="trace.zip")
+
+#def test_page_has_get_started_link(browser: Browser):
+#    context = browser.new_context(
+#        record_video_dir="video/"
+#    )
     page = context.new_page()
 
     page.goto("https://playwright.dev/python")
