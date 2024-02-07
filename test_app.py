356461681f3ee8d8a454b6a43a34a85f53aaf36a
simutek <simutek@Xavier-Mac-3.local>
2024-02-07 11:20:55 -0800

    test: takes video

--- test_app.py
+++ test_app.py
@@ -1,16 +1,23 @@
 #test_app.py
 
-from playwright.sync_api import Page
+import pytest
+from playwright.sync_api import Browser, Page
 
 DOCS_URL = "https://playwright.dev/python/docs/intro"
 
 
-def test_page_has_get_started_link(page: Page):
+def test_page_has_get_started_link(browser: Browser):
+    context = browser.new_context(
+        record_video_dir="video/"
+    )
+    page = context.new_page()
+
     page.goto("https://playwright.dev/python")
+
+    theme_btn = page.get_by_title("Switch between dark and light mode (currently dark mode)")
+    theme_btn.click()
 
     link = page.get_by_role("link", name="GET STARTED")
     link.click()
-    #to take an screenshot
-    page.screenshot(path="docs.jpg", full_page=True)
-
+    
     assert page.url == DOCS_URL
