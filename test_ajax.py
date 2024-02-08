d645536113d8e176c260db0c34bacf5c144f842b
simutek <simutek@Xavier-Mac-3.local>
2024-02-07 21:38:16 -0800

    test delay and ajax

---
+++ test_ajax.py
@@ -0,0 +1,13 @@
+#test_app.py
+#from playwright.sync_api import Page, expect
+
+#def test_ajax_data(page: Page):
+#page.goto("https://uitestingplayground.com/ajax")
+
+#btn = page.get_by_role("button", name="Button Triggering AJAX Request")
+#btn.click()
+
+#paragraph = page.locator("p.bg-success")
+#paragraph.wait_for()
+
+#expect(paragraph).to_be_visible()
