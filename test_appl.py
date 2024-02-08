14a78927a9e536df649535b83e9b717d19274d1d
simutek <simutek@Xavier-Mac-3.local>
2024-02-07 20:27:09 -0800

    test:input

--- test_appl.py
+++ test_appl.py
@@ -1,22 +1,26 @@
+#test_input.py
 from playwright.sync_api import Page, expect
 
-def test_page_has_get_started_link(page:Page):
+
+def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/python")
     input = page.get_by_placeholder("Search docs")
 
-    #input is hidden before btn click
+    # input is hidden initially
     expect(input).to_be_hidden()
 
-    #search btn
-    search_btn = page.get_by_role("button", name ="Search")
-    search_btn.press("Control+KeyK") #should pop the search menu
-
+    # search button
+    search_btn = page.get_by_role("button", name="Search")
+    search_btn.press("Control+KeyK") 
+    
+    # input should be visible/editable
     expect(input).to_be_editable()
+    # input should be empty initially
     expect(input).to_be_empty()
 
-    #type some query 
+    # type some query in the input
     query = "assertions"
     input.fill(query)
 
-    #verify input
+    # expect the input value as query
     expect(input).to_have_value(query)
